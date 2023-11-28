53aeb98866cfdb99d1914800cb017da3b0c43c0e
ihzhatamamy <ihzhatamamy@gmail.com>
2023-11-28 22:50:36 +0700

    update security filter chain

--- src/main/java/com/example/amigoscode/loginregistration/loginfromAmigoscode/security/config/SpringSecurity.java
+++ src/main/java/com/example/amigoscode/loginregistration/loginfromAmigoscode/security/config/SpringSecurity.java
@@ -2,8 +2,7 @@ package com.example.amigoscode.loginregistration.loginfromAmigoscode.security.co
 
 
 import com.example.amigoscode.loginregistration.loginfromAmigoscode.appuser.AppUserService;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,20 +13,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@RequiredArgsConstructor
+@AllArgsConstructor
 @EnableWebSecurity
-@NoArgsConstructor
 public class SpringSecurity {
 
     private final AppUserService appUserService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public SpringSecurity(AppUserService appUserService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.appUserService = appUserService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
